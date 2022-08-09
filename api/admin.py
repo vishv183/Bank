503ea96b2abd16677eaec7034b2835401bca44d8
Vishv <vishva@gmail.com>
2022-08-09 16:13:04 +0530

    create models: Account AccountType

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import *
 
+
+admin.site.register(Account)
+admin.site.register(AccountType)
 # Register your models here.
--- api/models.py
+++ api/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class AccountType(models.Model):
+    name = models.CharField(max_length=100)
+    yearly_intrest_rate = models.FloatField()
+    minimum_balance = models.FloatField()
+
+    class Meta:
+        ordering = ['id']
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Account(models.Model):
+    number = models.CharField(max_length=16)
+    ifsc = models.CharField(max_length=16)
+    balance = models.FloatField()
+    holder_name = models.CharField(max_length=200)
+    holder_email = models.CharField(max_length=200)
+    holder_phone = models.CharField(max_length=15)
+    holder_phone = models.CharField(max_length=15)
+    account_type = models.ForeignKey('AccountType', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['id']
+
+    def __str__(self):
+        return f'{self.number}  -  {self.holder_name}'
+
+
