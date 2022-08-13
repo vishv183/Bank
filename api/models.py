81d2e2fe957fe4460b3477f980274ffbb24355bf
Vishv <vishva@gmail.com>
2022-08-13 16:49:17 +0530

    made new models and migrated

--- api/models.py
+++ api/models.py
@@ -82,9 +82,79 @@ class Card(models.Model):
     card_type = models.ForeignKey('CardType', on_delete=models.CASCADE)
     account = models.ForeignKey('Account', on_delete=models.CASCADE)
     current_month_usage = models.FloatField()
+
+    class Meta:
+        ordering = ['id']
+
+    def __str__(self):
+        return f'{self.number} Expiry: {self.expiry_month}/{self.expiry_year} - {self.card_holder_name} (₹{self.current_month_usage}) | {self.card_type}'
+
+
+class Deposit(models.Model):
+    account = models.ForeignKey("Account", on_delete=models.CASCADE)
+    amount = models.FloatField()
+    description = models.TextField()
+    time_of_deposit = models.DateTimeField(auto_now_add=True)
     
     class Meta:
         ordering = ['id']
         
     def __str__(self):
-        return f'{self.number} Expiry: {self.expiry_month}/{self.expiry_year} - {self.card_holder_name} (₹{self.current_month_usage}) | {self.card_type}'
+        return f'{self.time_of_deposit} | {self.amount} - {self.account.number}'
+
+
+class Withdraw(models.Model):
+    account = models.ForeignKey("Account", on_delete=models.CASCADE)
+    amount = models.FloatField()
+    description = models.TextField()
+    time_of_withdraw = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['id']
+        
+    def __str__(self):
+        return f'{self.time_of_withdraw} | {self.amount} - {self.account.number}'
+
+
+class Transaction(models.Model):
+    sender_account = models.ForeignKey('Account', on_delete=models.CASCADE, related_name="Sender")
+    receiver_account = models.ForeignKey('Account', on_delete=models.CASCADE, related_name="Receiver")
+    amount = models.FloatField()
+    description = models.TextField()
+    time_of_transaction = models.DateTimeField(auto_now_add=True)
+    status = models.BooleanField()
+    
+    class Meta:
+        ordering = ['id']
+        
+    def __str__(self):
+        return f'{self.sender_account.number} sent {self.amount} to {self.receiver_account.number}'
+    
+
+class FixedDepositPlan(models.Model):
+    name = models.CharField(max_length=200)
+    miminum_amount = models.FloatField()
+    mimimum_term_in_year = models.FloatField()
+    maximum_term_in_year = models.FloatField()
+    intrest_per_anum = models.FloatField()
+    description = models.TextField()
+    
+    class Meta:
+        ordering = ['id']
+        
+    def __str__(self):
+        return self.name
+    
+class FixedDeposit(models.Model):
+    account = models.ForeignKey("Account", on_delete=models.CASCADE)
+    plan = models.ForeignKey('FixedDepositPlan', on_delete=models.CASCADE)
+    amount = models.FloatField()
+    time_of_initiation = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['id']
+        
+    def __str__(self):
+        return f'{self.account.number} - INR. {self.amount} | {self.plan.name}'
+
+    
