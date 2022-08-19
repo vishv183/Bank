01cfd326c623e3e79f35fa1ea1ff69b1e760dc85
Vishv <vishva@gmail.com>
2022-08-19 16:04:01 +0530

    deleted testing file

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
 
     # The home page
     path('', views.index, name='home'),
-    path('',views.index_testing, name='index-testing'),
 
     # Matches any html file
     re_path(r'^.*\.*', views.pages, name='pages'),
